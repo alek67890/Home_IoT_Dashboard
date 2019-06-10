3cc6e441bf10726f04ff2d9ea7fe8808e17ff63f
Aleksander Grzelak <alek678900@gmail.com>
2019-06-10 12:19:50 +0200

    Added MQTT support

--- MiAS/app.py
+++ MiAS/app.py
@@ -1,7 +1,32 @@
 from flask import Flask
+from flask_mqtt import Mqtt
 import models
 from config import Configuration
 
 
+mqtt = Mqtt()
+
 app = Flask(__name__)
 app.config.from_object(Configuration)
+mqtt.init_app(app)
+
+
+
+
+@mqtt.on_connect()
+def handle_connect(client, userdata, flags, rc):
+    mqtt.subscribe('POW1/stat/POWER')
+    mqtt.subscribe('POW1/tele/LWT')
+    mqtt.subscribe('POW1/tele/SENSOR')
+
+
+@mqtt.on_message()
+def handle_mqtt_message(client, userdata, message):
+    data = dict(
+        topic=message.topic,
+        payload=message.payload.decode()
+    )
+    print('topic =')
+    print(data['topic'])
+    print('payload =')
+    print(data['payload'])
--- MiAS/config.py
+++ MiAS/config.py
@@ -5,3 +5,9 @@ class Configuration(object):
     DEBUG = True
     ENV = 'development'
 
+    MQTT_BROKER_URL = os.getenv('MQTT_BROKER_URL', 'localhost')
+    MQTT_BROKER_PORT = int(os.getenv('MQTT_BROKER_PORT', '1883'))
+    MQTT_USERNAME = os.getenv('MQTT_USERNAME', '')
+    MQTT_PASSWORD = os.getenv('MQTT_PASSWORD', '')
+    MQTT_KEEPALIVE = 5  # set the time interval for sending a ping to the broker to 5 seconds
+    MQTT_TLS_ENABLED = False  # set TLS to disabled for testing purposes
