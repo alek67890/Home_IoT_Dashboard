1a909e3a7a6e367076bd225882cda9d1fd375b5b
Aleksander Grzelak <alek678900@gmail.com>
2019-06-11 00:14:50 +0200

    Dashboard + device buuton states

--- MiAS/views.py
+++ MiAS/views.py
@@ -6,9 +6,13 @@ from devices import objects
 @app.route('/')
 @app.route('/dashboard')
 def dashboard():
-    device = request.args.get('device')
+    device = request.args.get('device', default="home")
+    toggle = request.args.get('toggle', default=None)
+    if device == "home":
+        return render_template('dashboard.html', topic_list=objects.list_of_topics(), data=objects.data, active=device)
+    else:
+        return render_template('dash_device.html', topic_list=objects.list_of_topics(), data=objects.data, active=device)
 
-    return render_template('dashboard.html', topic_list=objects.list_of_topics(), data=objects.data, active=device)
 
 
 @app.route('/ok')
