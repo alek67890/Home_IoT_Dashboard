bfe94ae2258a7aa28675c76ec2b37036405607d2
Aleksander Grzelak <alek678900@gmail.com>
2019-06-11 01:32:58 +0200

    add method to publish in MQTT

--- MiAS/views.py
+++ MiAS/views.py
@@ -1,4 +1,4 @@
-from app import app
+from app import app, mqtt
 from flask import abort, request, render_template
 from devices import objects
 
@@ -8,17 +8,33 @@ from devices import objects
 def dashboard():
     device = request.args.get('device', default='home')
     toggle = request.args.get('toggle', default=None)
+
     if device not in objects.list_of_topics():
         abort(404)
+
     if device == 'home':
         return render_template('dashboard.html', topic_list=objects.list_of_topics(),
                                data=objects.data, active=device)
+
     elif objects.type_device(device) in ["DeviceSensor"]:
+        change_state(toggle, device)
         return render_template('dash_device.html', topic_list=objects.list_of_topics(),
                                data=objects.data, active=device, power=True)
+
     else:
+        change_state(toggle, device)
         return render_template('dash_device.html', topic_list=objects.list_of_topics(),
                                data=objects.data, active=device)
+
+
+def change_state(toggle, device):
+    txt = device + '/cmnd/power'
+    if toggle == 'on':
+        objects.data[device].on()
+        mqtt.publish(txt, 'on')
+    if toggle == 'off':
+        objects.data[device].off()
+        mqtt.publish(txt, 'off')
 
 
 @app.route('/ok')
