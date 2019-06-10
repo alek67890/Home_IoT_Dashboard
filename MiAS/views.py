5ae2c7d0f50bca32b09643402a0af832bc8590b9
Aleksander Grzelak <alek678900@gmail.com>
2019-06-11 00:24:14 +0200

    Add support for error 404

--- MiAS/views.py
+++ MiAS/views.py
@@ -6,18 +6,24 @@ from devices import objects
 @app.route('/')
 @app.route('/dashboard')
 def dashboard():
-    device = request.args.get('device', default="home")
+    device = request.args.get('device', default='home')
     toggle = request.args.get('toggle', default=None)
-    if device == "home":
+    if device not in objects.list_of_topics():
+        abort(404)
+    if device == 'home':
         return render_template('dashboard.html', topic_list=objects.list_of_topics(), data=objects.data, active=device)
     else:
         return render_template('dash_device.html', topic_list=objects.list_of_topics(), data=objects.data, active=device)
 
 
-
 @app.route('/ok')
 def ok():
     return('OK')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html', topic_list=objects.list_of_topics(), data=objects.data,), 404
 
 
 
