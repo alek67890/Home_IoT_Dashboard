8556d86c2ddecf09d1757645ad151bf9755823c5
Aleksander Grzelak <alek678900@gmail.com>
2019-06-10 23:31:36 +0200

    Working navbar

--- MiAS/views.py
+++ MiAS/views.py
@@ -1,10 +1,14 @@
 from app import app
 from flask import abort, request, render_template
+from devices import devices
+
 
 @app.route('/')
 @app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html')
+    device = request.args.get('device')
+
+    return render_template('dashboard.html', topic_list=devices.list_of_topics(), data=devices.data, active=device)
 
 
 @app.route('/ok')
